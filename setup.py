6808b6867771523917c3c299b8399fcdce0a9b5d
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2021-07-06 21:30:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name='BiGramGraph',  # How you named your package folder (MyLib)
     packages=['BiGramGraph'],  # Chose the same as "name"
-    version='v1',  # Start with a small number and increase it with every change you make
+    version='v2',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Implementations of concepts presented in the paper "On Bi Gram Graph attributes"',
     # Give a short description about your library
     author='Thomas Konstantinovsky and Matan Mizrachi',  # Type in your name
     author_email='thomaskon90@gmail.com, matt8ac@gmail.com',  # Type in your E-Mail
     url='https://github.com/MuteJester/BiGramGraph',  # Provide either the link to your github or to your website
-    download_url='https://github.com/MuteJester/BiGramGraph/archive/refs/tags/v1.tar.gz',  # I explain this later on
+    download_url='https://github.com/MuteJester/BiGramGraph/archive/refs/tags/v2.tar.gz',  # I explain this later on
     keywords=['NLP', 'Machine Learning', 'Graph Theory', 'Text Generation', 'Metrics'],
     # Keywords that define your package best
     install_requires=[  # I get to this in a second
